ee9a5e43452f894919b91bca5eab72d18a8b8a12
cminnerath <cminnerath@gmail.com>
2016-01-11 02:56:25 -0700

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :minis
   resource :sessions, :only => [:create]
 
-  root to: 'minis#index'
-  get '/auth/:provider/callback' => 'sessions#create'
+  root to: 'site#index'
+
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/login"  => redirect("/auth/google_oauth2"), as: :login, via: [:get, :post]
+  delete "/logout" => "sessions#destroy", as: :logout
 end
