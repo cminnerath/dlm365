076b443a95c73c4b2bb17788a02c0092e6fd43c8
cminnerath <cminnerath@gmail.com>
2016-01-11 20:05:40 -0700

    add imdb service

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -3,7 +3,6 @@ module ImdbService
   def self.grab(imdb_id)
     imdb = ImdbParty::Imdb.new(:anonymize => true)
     movie = imdb.find_movie_by_id(imdb_id)
-    parse(movie)
   end
 
   def self.parse(movie)
