4673ac86d1bd16f2266c40ef07310da2b5b6eede
cminnerath <cminnerath@gmail.com>
2016-04-27 00:33:55 -0600

    allow cross domain requests for authentication
    * add cross origin resource sharing method to aplication_controller
    
    * add bad_token sad path to application_controller in case of expiry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_filter :cor
+  rescue_from ActionController::InvalidAuthenticityToken, :with => :bad_token
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -15,4 +17,15 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 
+  def cor
+    headers["Access-Control-Allow-Origin"]  = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
+  def bad_token
+    flash[:warning] = "Session expired"
+    redirect_to root_path
+  end
 end
